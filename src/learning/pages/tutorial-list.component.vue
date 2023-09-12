8476ec5f74af244c2fec0385116c2728febb24b6
Adrian Melgar <DaikiMT@outlook.es>
2023-09-11 20:56:09 -0500

    Something idk

---
+++ src/learning/pages/tutorial-list.component.vue
@@ -0,0 +1,41 @@
+<template>
+
+</template>
+
+<script>
+import {TutorialsApiServices} from "../services/tutorials-api.services.js";
+
+export default {
+  name: "tutorial-list",
+  title: "Tutorials",
+  data() {
+    return {
+      tutorial: [],
+      tutorialDialog: false,
+      deleteTutorialDialog: false,
+      deleteTutorialsDialog: false,
+      tutorials: {},
+      selectedTutorial: {},
+      filters: {},
+      submitted: false,
+      statuses: [
+        { label: "Published", value: "published" },
+        { label: "Unpublished", value: "unpublished" },
+        ],
+        tutorialService: null,
+    }
+  },
+    created() {
+      this.tutorialService = new TutorialsApiServices();
+      this.tutorialService.getAll()
+          .then(response => {
+            this.tutorials = response.data;
+            console.log(response.data);
+          })
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
